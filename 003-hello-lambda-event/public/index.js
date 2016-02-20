1f013a86a51dec4a2800187a709c0cf3fb05ab4b
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2016-02-20 17:10:20 +0100

    Add console.log statements

--- 003-hello-lambda-event/public/index.js
+++ 003-hello-lambda-event/public/index.js
@@ -2,5 +2,7 @@
 
 exports.handler = function (event, context) {
   event.name = event.name || "unknown Lambda tester";
+
+  console.log("About to succeed with event.name:", event.name);
   context.succeed("Hello, " + event.name + "!!!");
 };
