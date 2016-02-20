46d9216d5c14d96c440027fb7cee15770649c9a1
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2016-02-20 13:01:13 +0100

    Add Hello Lambda example

---
+++ 001-hello-lambda/public/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+  exports:
+    The method associated to exports (in this case `handler`), is the one will be
+    called by Lambda when the function is activated .
+    It does not have to be called `handler`, but it's the recommended practice.
+
+  function (event, context):
+    Then method received two args.
+    `event` is the object that contains details
+    about the executing event (for example, when it is S3 related you will find
+    the bucket and the key).
+    `context` contains internal information about the function itself, as well
+    as the methods for ending the function (`succeed`).
+    The argument passed to `succeed` will be returned to the invoker.
+*/
+exports.handler = function (event, context) {
+  context.succeed("Hello, world");
+};
